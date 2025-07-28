1c461d0a0c322b81a1c7d667815662d68b0cc751
Suryakant Singh <surykntsingh@gmail.com>
2025-07-28 19:50:15 -0400

    modified dataset

--- train_gecko.py
+++ train_gecko.py
@@ -96,7 +96,8 @@ class GeckoDataset(Dataset):
         bag_feats_deep = self.__read_h5(slide_id)
         # bag_feats_deep = bag_feats_deep.view(-1, self.feats_size_deep)
         
-        bag_feats = pd.read_csv(f'{self.features_path}/{slide_id}.csv')
+        # bag_feats = pd.read_csv(f'{self.features_path}/{slide_id}.csv')
+        bag_feats = torch.tensor(np.array(pd.read_csv(f'{self.features_path}/{slide_id}.csv')))
         # bag_feats = bag_feats.view(-1, self.feats_size)
 
         bag_feats = self.__normalize_feature(bag_feats)
