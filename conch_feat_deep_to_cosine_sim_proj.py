56f387e0c4a4dded6e2236cab599b57cfcf293b1
Suryakant Singh <surykntsingh@gmail.com>
2025-09-25 14:04:46 -0400

    updated script

--- conch_feat_deep_to_cosine_sim_proj.py
+++ conch_feat_deep_to_cosine_sim_proj.py
@@ -14,20 +14,27 @@ from tqdm import tqdm
 import pickle
 import argparse
 import torch.nn.functional as F
-
+parser = argparse.ArgumentParser()
 sys.path.insert(1, 'GECKO/data_curation/CONCH')
 
 from conch.open_clip_custom import create_model_from_pretrained
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model, preprocess = create_model_from_pretrained('conch_ViT-B-16', 'CONCH/checkpoints/conch/pytorch_model.bin', device=device)
+parser = argparse.ArgumentParser()
+parser.add_argument('--save_path', type=str, default = "/mnt/surya/dataset/sbu/processed_features/20x_512px_0px_overlap")
+parser.add_argument('--feature_dir', type=str, default = "/mnt/surya/dataset/sbu/processed_features/20x_512px_0px_overlap/features_conch_v1")
+parser.add_argument('--conch_model_type', type=str, default = 'conch_ViT-B-16')
+parser.add_argument('--conch_model_path', type=str, default = 'CONCH/checkpoints/conch/pytorch_model.bin')
+
+args = parser.parse_args()
+
+
+model, preprocess = create_model_from_pretrained('conch_ViT-B-16', args.conch_model_path, device=device)
 _ = model.eval()
 
-save_path = '/mnt/saarthak/datasets/REG_test2_revised_processed/20x_512px_0px_overlap/features_conch_v1_cosine_sim_proj'
-
-for path in tqdm(sorted(os.listdir('/mnt/saarthak/datasets/REG_test2_revised_processed/20x_512px_0px_overlap/features_conch_v1'))[:]):
-    with h5py.File(f'/mnt/saarthak/datasets/REG_test2_revised_processed/20x_512px_0px_overlap/features_conch_v1/{path}', 'r') as f:
+for path in tqdm(sorted(os.listdir(''))[:]):
+    with h5py.File(f'/{args.feature_dir}/{path}', 'r') as f:
         features = f['features'][:]
         pooled = F.normalize(torch.Tensor(features).cuda() @ model.visual.proj_contrast, dim=-1)
-        torch.save(pooled, f'{save_path}/{path.replace(".h5", ".pt")}')
+        torch.save(pooled, f'{args.save_path}/{path.replace(".h5", ".pt")}')
